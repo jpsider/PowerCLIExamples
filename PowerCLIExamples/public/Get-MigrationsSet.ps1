0d534b983d62645c79fd787f789c86f6641114a4
Dan Barr <dan@dbarr.com>
2019-08-26 22:00:35 -0700

    Add comment help and clear trailing whitespace

--- PowerCLIExamples/public/Get-MigrationsSet.ps1
+++ PowerCLIExamples/public/Get-MigrationsSet.ps1
@@ -1,13 +1,17 @@
 <#
-Script name: get-migrations.ps1
-Created on: 20/12/2018
-Author: Chris Bradshaw @aldershotchris
-Description: The purpose of the script is to list the currently running + recently finished VM migrations.
-Dependencies: None known
+.DESCRIPTION
+    Lists the currently running + recently finished VM migrations
+.EXAMPLE
+    PS C:\> Get-MigrationsSet
+    Lists current and recent VM migration tasks
+.NOTES
+    Created on: 20/12/2018
+    Author: Chris Bradshaw @aldershotchris
 #>
+
 Function Get-MigrationsSet
 {
-    Get-Task | 
-    Where-Object { $_.Name -eq "RelocateVM_Task" } | 
+    Get-Task |
+    Where-Object { $_.Name -eq "RelocateVM_Task" } |
     Select-Object @{Name = "VM"; Expression = { Get-VM -Id $_.ObjectID } }, State, @{Name = "% Complete"; Expression = { $_.PercentComplete } }, StartTime
 }
