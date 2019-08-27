cb19be2aad35f9b0599972ee8308849eb416f889
jpsider <jpsider@gmail.com>
2019-08-26 23:00:39 -0400

    updated hardcoded path.

--- tests/Private/Disable-SSLValidation.Tests.ps1
+++ tests/Private/Disable-SSLValidation.Tests.ps1
@@ -1,4 +1,4 @@
-$script:ModuleName = 'RestPS'
+$script:ModuleName = 'PowerCLIExamples'
 
 $here = (Split-Path -Parent $MyInvocation.MyCommand.Path) -replace 'tests', "$script:ModuleName"
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
